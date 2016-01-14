9e278f846280dbcafbb12112d3c9e5c63b4cf6be
Kyle Minshall <kyleminshall@gmail.com>
2016-01-14 15:50:38 -0800

    Adding back sleepelem

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -141,7 +141,7 @@ thread_tick (void)
   
   struct list_elem *e;
   for(e = list_begin(&sleep_list); e != list_end(&sleep_list); e = list_next(e)) {
-    struct thread *t = list_entry (e, struct thread, elem);
+    struct thread *t = list_entry (e, struct thread, sleepelem);
     if(t->end_tick == 0) {
       thread_unblock(t);
     } else {
--- src/threads/thread.h
+++ src/threads/thread.h
@@ -92,6 +92,8 @@ struct thread
 
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
+ 
+    struct list_elem sleepelem;		/* List element for sleeping threads list. */
 
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
