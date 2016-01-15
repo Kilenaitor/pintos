f5efd9f438b868bbca0cdbb02a3e7dcb9f96ee3f
Kyle Minshall <kyleminshall@gmail.com>
2016-01-15 11:46:30 -0800

    For some reason printfs aren't working

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -261,8 +261,8 @@ thread_sleep (void)
   
   if(t != idle_thread) {
     
-    printf("Thread ticks are: %d\n", t->end_tick);
-    printf("Thread magic is %d\n", t->magic);
+//    printf("Thread ticks are: %d\n", t->end_tick);
+//    printf("Thread magic is %d\n", t->magic);
     
     list_push_back(&sleep_list, &t->elem); //Append it to list of sleepy threads
   }
