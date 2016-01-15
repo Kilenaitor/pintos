93ec12f6d85f6ae7fb9f508d1efb94f15e63991d
Kyle Minshall <kyleminshall@gmail.com>
2016-01-14 16:19:36 -0800

    Debugging.

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -145,10 +145,12 @@ thread_tick (void)
   for(e = list_begin(&sleep_list); e != list_end(&sleep_list); e = list_next(e)) {
     struct thread *tmp_elem = list_entry (e, struct thread, sleepelem);
     if(tmp_elem->end_tick == 0) {
+      printf("End tick value is %s", tmp_elem->end_tick);
       ASSERT(tmp_elem != NULL);
       ASSERT(tmp_elem->magic != NULL);
       thread_unblock(tmp_elem);
-    } else {
+    } else { 
+        printf("Decrementing by 1");
         tmp_elem->end_tick -= 1;
     }
   }	
@@ -244,8 +246,9 @@ thread_block (void)
   ASSERT (intr_get_level () == INTR_OFF);
 
   struct thread *t = thread_current (); //Grabbing current thread
-  list_push_back(&sleep_list, &t->elem); //Append it to list of sleepy threads
   t->status = THREAD_BLOCKED;
+  
+  list_push_back(&sleep_list, &t->elem); //Append it to list of sleepy threads
   
   schedule ();
 }
