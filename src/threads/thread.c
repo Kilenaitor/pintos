2d2981582e61825f62540d7a5640b4ccf3707007
Kyle Minshall <kyleminshall@gmail.com>
2016-01-15 11:52:33 -0800

    I am not convinced that OSes are just straight up black magic

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -144,7 +144,7 @@ thread_tick (void)
   for(e = list_begin(&sleep_list); e != list_end(&sleep_list); e = list_next(e)) {
     struct thread *tmp_elem = list_entry (e, struct thread, elem);
     
-    printf("Current thread is: %s", tmp_elem->name);
+    printf("Current thread is: %s\n", tmp_elem->name);
     ASSERT (tmp_elem->status == THREAD_BLOCKED);
     
     if(tmp_elem->end_tick > 0) {
@@ -154,6 +154,7 @@ thread_tick (void)
       printf("End tick value is %d\n", tmp_elem->end_tick);
       printf("Magic is %d\n", tmp_elem->magic);
       thread_unblock(tmp_elem);
+      list_remove(e);
     }
   }	
 
