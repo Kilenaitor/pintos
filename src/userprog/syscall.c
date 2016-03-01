1743b5117afeb3f5bee82b0e16c11486f1eaa776
Joshua Wong <joshuahwong96@gmail.com>
2016-02-29 23:30:47 -0800

    Added user memory access checking and syscall structure

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,6 +3,7 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include <"userprog/pagedir.h">
 
 static void syscall_handler (struct intr_frame *);
 
@@ -12,9 +13,166 @@ syscall_init (void)
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
-static void
-syscall_handler (struct intr_frame *f UNUSED) 
+// Checks if user pointer is 
+static bool valid_usr_ptr(uint32_t *pd, const void *uaddr)
 {
-  printf ("system call!\n");
-  thread_exit ();
+  // Checks if uaddr is below PHYS_BASE, above 0x08048000 (address given in directions), and not unmapped
+  if(is_user_vaddr (addr) && addr >= 0x08048000 && pagedir_get_page (pd, uaddr) != NULL)
+    {
+      return true;
+    }
+  return false;
+}
+
+static void
+syscall_halt (struct intr_frame *f)
+{
+}
+
+
+static void
+syscall_exit (int status)
+{
+}
+
+static void
+syscall_exec (struct intr_frame *f)
+{
+}
+
+static void
+syscall_wait (struct intr_frame *f)
+{
+}
+
+static void
+syscall_create (struct intr_frame *f)
+{
+}
+
+static void
+syscall_remove (struct intr_frame *f)
+{
+}
+
+static void
+syscall_open (struct intr_frame *f)
+{
+}
+
+static void
+syscall_filesize (struct intr_frame *f)
+{
+}
+
+static void
+syscall_read (struct intr_frame *f)
+{
+}
+
+static void
+syscall_write (struct intr_frame *f)
+{
+}
+
+static void
+syscall_seek (struct intr_frame *f)
+{
+}
+
+static void
+syscall_tell (struct intr_frame *f)
+{
+}
+
+static void
+syscall_close (struct intr_frame *f)
+{
+}
+
+static void
+syscall_handler (struct intr_frame *f) 
+{
+  // Check if esp is valid
+
+  if(!valid_usr_ptr(current_thread ()->pagedir, f->esp))
+    {
+      syscall_exit(-1);
+    }
+  switch (*(f->esp))
+    {
+      case SYS_HALT:
+        {
+          syscall_halt(f);
+          break;
+        }
+      case SYS_EXIT:
+        {
+          syscall_exit(0);
+          break;
+        }
+      case SYS_EXEC:
+        {
+          syscall_exec(f);
+          break;
+        }
+      case SYS_WAIT:
+        {
+          syscall_wait(f);
+          break;
+        }
+      case SYS_CREATE:
+        {
+          syscall_create(f);
+          break;
+        }
+      case SYS_REMOVE:
+        {
+          syscall_remove(f);
+          break;
+        }
+      case SYS_OPEN:
+        {
+          syscall_open(f);
+          break;
+        }
+      case SYS_FILESIZE:
+        {
+          syscall_filesize(f);
+          break;
+        }
+      case SYS_READ:
+        {
+          syscall_read(f);
+          break;
+        }
+      case SYS_WRITE:
+        {
+          syscall_write(f);
+          break;
+        }
+      case SYS_SEEK:
+        {
+          syscall_seek(f);
+          break;
+        }
+      case SYS_TELL:
+        {
+          syscall_tell(f);
+          break;
+        }
+      case SYS_CLOSE:
+        {
+          syscall_close(f);
+          break;
+        }
+      default:
+        {
+          syscall_exit(-1);
+          break;
+        }
+    }
+
+  //printf ("system call!\n");
+  //thread_exit ();
 }
